5b327376dec761ff991d1edd713a8c947f6b6e9a
Jake Jepson <jwjepson1@gmail.com>
2022-09-02 16:15:40 -0400

    get user input & add new book into library array

--- scripts.js
+++ scripts.js
@@ -6,3 +6,15 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+function addBookToLibrary(title, author, pages) {
+    myLibrary.push(new Book(title, author, pages));
+}
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", () => {
+    const title = document.querySelector("#title").value; 
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    addBookToLibrary(title, author, pages);
+});
