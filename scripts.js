7e20fadf23c54a2f5df4b224fe862b0db1960461
Jake Jepson <jwjepson1@gmail.com>
2022-09-02 20:50:47 -0400

    display and close overlay & create reset form function

--- scripts.js
+++ scripts.js
@@ -16,5 +16,28 @@ submitButton.addEventListener("click", () => {
     const title = document.querySelector("#title").value; 
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    addBookToLibrary(title, author, pages);
+    const read = document.querySelector("#read-it")
+    console.log(title, author, pages, read);
+    addBookToLibrary(title, author, pages, read);
+});
+
+function resetForm() {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read = false;
+
+}
+
+newBookButton = document.querySelector("#newBookButton");
+overlay = document.querySelector(".overlay");
+closeButton = document.querySelector(".close-button");
+
+newBookButton.addEventListener("click", () => {
+    overlay.style.display = "flex";
+});
+
+closeButton.addEventListener("click", () => {
+    overlay.style.display = "none";
+    resetForm();
 });
