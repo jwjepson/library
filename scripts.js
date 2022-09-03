b4b8e26308231589fccefcf86796bf05438608d2
Jake Jepson <jwjepson1@gmail.com>
2022-09-02 23:46:52 -0400

    append each books data to a card

--- scripts.js
+++ scripts.js
@@ -17,6 +17,27 @@ function displayBooks() {
     myLibrary.forEach((book) => {
         const card = document.createElement("div");
         card.classList.add("card");
+
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        const author = document.createElement("div");
+        author.classList.add("author");
+        author.textContent = `By ${book.author}`;
+        card.appendChild(author);
+
+        const pages = document.createElement("div");
+        pages.classList.add("pages");
+        pages.textContent = `${book.pages} pages`;
+        card.appendChild(pages);
+
+        const read = document.createElement("button");
+        read.classList.add("readButton");
+        read.textContent = book.read;
+        card.appendChild(read);
+
         library.appendChild(card);
     });
 };
@@ -26,7 +47,13 @@ submitButton.addEventListener("click", () => {
     const title = document.querySelector("#title").value; 
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read-it").checked;
+    let read = document.querySelector("#read-it").checked;
+    if (read == true) {
+        read = "read";
+    }
+    else {
+        read = "not read";
+    }
     addBookToLibrary(title, author, pages, read);
     overlay.style.display = "none";
     displayBooks();
